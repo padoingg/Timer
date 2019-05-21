76d613e8325181d61f8ef9540a1f5cd4ccc91bc5
Namhee Kang <padoingg@gmail.com>
2019-05-21 14:40:55 +0900

    #1 두 개의 텍스트 박스와 1개의 버튼, 타이머를 추가하여 속성을 설정함.

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Timer
 {
-    partial class Form1
+    partial class Name
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -29,12 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txtCountDown = new System.Windows.Forms.TextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(13, 13);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(233, 21);
+            this.txtNum.TabIndex = 0;
+            this.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(13, 40);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(233, 81);
+            this.btnCount.TabIndex = 1;
+            this.btnCount.Text = "카운트다운";
+            this.btnCount.UseVisualStyleBackColor = true;
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.Font = new System.Drawing.Font("굴림", 20F);
+            this.txtCountDown.Location = new System.Drawing.Point(13, 127);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.ReadOnly = true;
+            this.txtCountDown.Size = new System.Drawing.Size(233, 38);
+            this.txtCountDown.TabIndex = 2;
+            this.txtCountDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            // 
+            // Name
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(258, 176);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtNum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Name";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtCountDown;
+        private System.Windows.Forms.Timer Timer;
     }
 }
 
--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Timer
 {
-    public partial class Form1 : Form
+    public partial class Name : Form
     {
-        public Form1()
+        public Name()
         {
             InitializeComponent();
         }
